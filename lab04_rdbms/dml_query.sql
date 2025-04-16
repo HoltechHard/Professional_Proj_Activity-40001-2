e170fd6b1efd8cb7aded3226da414893d555fb11
Holger <hadronx@live.com>
2025-04-16 09:58:07 +0300

    dml queries

---
+++ lab04_rdbms/dml_query.sql
@@ -0,0 +1,52 @@
+-- Database: adventure_works_oltp
+
+---------------------------
+--		DML QUERIES 	-- 
+---------------------------
+
+-- 1) list of subcategories
+select sc.productsubcategoryid, sc.productcategoryid, sc.name
+from "production".productsubcategory as sc;
+
+-- 2) list of products
+select pr.productid, pr.name
+from "production".product as pr;
+
+-- 3) list of purchase order heads
+select ord.purchaseorderid, ord.vendorid, ord.employeeid, ord.subtotal
+from "purchasing".purchaseorderheader ord;
+
+-- 4) list of purchase order details
+select pod.purchaseorderid, pod.purchaseorderdetailid, pod.productid, pod.orderqty, pod.unitprice 
+from "purchasing".purchaseorderdetail as pod;
+
+-- 5) list with join of tables: order details, product and subcategory
+select sc.name as subcategory, pr.name as product, pod.orderqty, pod.unitprice
+from "purchasing".purchaseorderdetail as pod
+inner join "production".product as pr
+on pod.productid = pr.productid 
+inner join "production".productsubcategory as sc
+on pr.productsubcategoryid = sc.productsubcategoryid;
+
+-- 6) group of number of products by subcategory
+select sc.name, count(pr.productid)
+from "production".productsubcategory as sc
+inner join "production".product as pr
+on sc.productsubcategoryid = pr.productsubcategoryid
+group by sc.name;
+
+-- 7) group of total amount of purchases by product
+select pr.name, sum(pod.orderqty * pod.unitprice) 
+from "purchasing".purchaseorderdetail as pod
+inner join "production".product as pr
+on pod.productid = pr.productid
+group by pr.name;
+
+-- 8) total of products
+select count(pr.productid) 
+from "production".product as pr;
+
+-- 9) total amount of purchases
+select sum(pod.subtotal)
+from "purchasing".purchaseorderheader as pod;
+
